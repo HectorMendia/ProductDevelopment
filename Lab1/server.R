8a4c2818f01b340d11d6626d37c0abaa17ee0e50
Hector Mendia <hector.mendia@gmail.com>
2020-10-03 08:45:40 -0600

    Tarea 1

---
+++ Lab1/server.R
@@ -0,0 +1,44 @@
+#
+# This is the server logic of a Shiny web application. You can run the
+# application by clicking 'Run App' above.
+#
+# Find out more about building applications with Shiny here:
+#
+#    http://shiny.rstudio.com/
+#
+
+library(shiny)
+
+# Define server logic required to draw a histogram
+shinyServer(function(input, output) {
+    contador <- reactiveValues(cont1 = 0, cont2 = 0)
+    #contador2 <- reactiveValues(cont2 = 0)
+    output$input1 <- renderText(paste0(c('Output slider input: ', input$input1), collapse = ''))
+    output$input2 <- renderText(input$input2)
+    output$input3 <- renderText(input$input3)
+    output$input4 <- renderText({
+        listaValores <- paste0(input$input4, collapse = ', ')
+        paste0(c("Selecciones del UI: ", listaValores), collapse = "")
+    })
+    output$input5 <- renderText(as.character(input$input5))
+    output$input6 <- renderText(as.character(input$input6))
+    output$input7 <- renderText(input$input7)
+
+    output$input8 <- renderText(input$input8)
+    output$input9 <- renderText(input$input9)
+    output$input10 <- renderText(input$input10)
+    output$input11 <- renderText(input$input11)
+    output$input12 <- renderText(input$input12)
+    output$input13 <- renderText(input$input13)
+
+    observeEvent(input$btn1, {
+        contador$cont1 <- contador$cont1 + 1
+    })
+    output$btn1 <- renderText(contador$cont1)
+    
+    observeEvent(input$btn2, {
+        contador$cont2 <- contador$cont2 + 1
+    })
+    output$btn2 <- renderText(contador$cont2)
+    
+})
---
+++ Lab1/ui.R
@@ -0,0 +1,124 @@
+#
+# This is the user-interface definition of a Shiny web application. You can
+# run the application by clicking 'Run App' above.
+#
+# Find out more about building applications with Shiny here:
+#
+#    http://shiny.rstudio.com/
+#
+
+library(shiny)
+
+# Define UI for application that draws a histogram
+shinyUI(
+    fluidPage(
+        # Application title
+        titlePanel("Inputs en Shiny"),
+        tabsetPanel(
+        tabPanel("Input Examples",
+            fluidPage(
+
+    
+            sidebarLayout(
+                sidebarPanel(
+                    sliderInput("input1",
+                                "Seleccione valor",
+                                min = 0,
+                                max = 100,
+                                value = 50,
+                                step = 10,
+                                animate = TRUE, post = '%'),
+                    sliderInput("input2",
+                                "Seleccione un rango",
+                                min = 0,
+                                max = 200,
+                                value = c(0, 50),
+                                step = 10),
+
+                    selectInput('input3', 'Seleccione un auto:', rownames(mtcars),
+                                'Duster 360'),
+                    selectInput('input4', 'Seleccione un autos:', rownames(mtcars),
+                                'Duster 360', multiple = TRUE),
+
+                    dateInput('input5', 'Ingrese la fecha:', value = Sys.Date() - 1, format = "yyyy-mm-dd"),
+                    dateRangeInput("input6", "Ingrese Fechas",
+                                start = Sys.Date() - 10,
+                                end = Sys.Date() + 10,
+                                min = Sys.Date() - 40,
+                                max = Sys.Date() + 40,
+                                format = "yyyy-mm-dd",
+                                separator = " to "),
+
+                    numericInput("input7", "Ingrese un numero:", 
+                        value = 10, min = 0, max = 100, step = 1),
+
+                    checkboxInput("input8", "Seleccione si verdadero", FALSE),
+                    checkboxGroupInput("input9", "Seleccione opciones", 
+                        #c("A" = "a", "B" = "b", "C" = "c", "D" = "d", "E" = "e")
+                        choices = LETTERS[1:5]
+                        ),
+                    radioButtons("input10", "Ingrese Genero", c("masculino" = "m", "femenino" = "f")),
+
+
+                    textInput("input11", "Ingrese texto"),
+
+
+                    textAreaInput("input12", "Ingrese parrafo"),
+                    passwordInput("input13", "Password"),
+
+                    actionButton("btn1", "ok"),
+                    actionLink("btn2", "siguiente"),
+                    submitButton("Reprocesar"),
+
+            ),
+                    mainPanel(
+                        h2("Slider input sencillo"),
+                        verbatimTextOutput("input1"),
+
+                        h2("Slider input rango"),
+                        verbatimTextOutput("input2"),
+
+                        h2("Select input"),
+                        verbatimTextOutput("input3"),
+
+                        h2("Select Input Multiple"),
+                        verbatimTextOutput("input4"),
+
+                        h2("Fecha"),
+                        verbatimTextOutput("input5"),
+
+                        h2("Rango de fechas"),
+                        verbatimTextOutput("input6"),
+
+                        h2("Numeric Input"),
+                        verbatimTextOutput("input7"),
+
+                        h2("Single Checkbox"),
+                        verbatimTextOutput("input8"),
+
+                        h2("Grouped checkbox"),
+                        verbatimTextOutput("input9"),
+
+                        h2("Radio Buttons"),
+                        verbatimTextOutput("input10"),
+
+                        h2("Texto"),
+                        verbatimTextOutput("input11"),
+
+                        h2("Parrafo"),
+                        verbatimTextOutput("input12"),
+                        h2("Password"),
+                        verbatimTextOutput("input13"),
+
+
+                        h2("Action Button"),
+                        verbatimTextOutput("btn1"),
+                        h2("Action Link"),
+                        verbatimTextOutput("btn2"),
+
+                )
+            ))),
+    tabPanel("Cargar Archivo", "contents")
+
+    )
+))
