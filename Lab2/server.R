e50857f90503d7a1698e055b4a455ec9c0f6db1d
Hector Mendia <hector.mendia@gmail.com>
2020-11-06 21:53:06 -0600

    Lab 2: avances

--- Lab2/server.R
+++ Lab2/server.R
@@ -1,159 +1,77 @@
 library(shiny)
-library(ggplot2)
+library(DT)
 library(dplyr)
-
-out_click<- NULL
-out_hover<-NULL
+library(ggplot2)
 
 shinyServer(function(input, output) {
 
-output$grafica_base_r <- renderPlot({
-    plot(mtcars$wt,mtcars$mpg,xlab="wt",ylab="Miler per Galon")
-})
-
-output$grafica_ggplot <- renderPlot({
-    diamonds %>% 
-        ggplot(aes(x=carat,y=price, color=color))+
-        geom_point()+
-        ylab("Precio")+
-        xlab("Kilates")+
-        ggtitle("Precio diamantes")
-})
-
-
-output$click_base_plot <- renderPlot({
-    plot(mtcars$wt,mtcars$mpg,xlab="wt",ylab="Miler per Galon")
+    #output$plot_click_options <- renderPlot({
+    #    plot(mtcars$wt, mtcars$mpg, xlab="Precio del vehiculo", ylab = "millas por galon")
+    #})
+    outParam <- NULL
+    outParamHover <- NULL
     
-})
+    selectedP <- reactive({
+        #print("--->")
+        #print(outParam)
+        if (!is.null(input$clk$x)){
+            df<-nearPoints(mtcars,input$clk,xvar='wt',yvar='mpg')
+            out <- df %>% 
+                select(wt,mpg)
+            outParam <<- rbind(outParam,out) %>% distinct()
+            print(out)
+            return(out)
+        }
 
-output$click_base_plot_xy <- renderPrint({
-    c(input$plot_click$x, input$plot_click$y)
-})
-
-
-output$click_ggplot <- renderPlot({
-    diamonds %>% 
-        ggplot(aes(x=carat,y=price, color=color))+
-        geom_point()+
-        ylab("Precio")+
-        xlab("Kilates")+
-        ggtitle("Precio diamantes")
+        if(!is.null(input$dclk$x)){
+            df<-nearPoints(mtcars,input$dclk,xvar='wt',yvar='mpg')
+            out <- df %>% 
+                select(wt,mpg)
+            outParam <<- setdiff(outParam,out)
+            return(out)
+        }
+        
+        if(!is.null(input$mhover$x)){
+            df<-nearPoints(mtcars,input$mhover,xvar='wt',yvar='mpg')
+            out <- df %>% 
+                select(wt,mpg)
+            outParamHover <<- out
+            return(outParamHover)
+        }
+        
+        if(!is.null(input$mbrush)){
+            df<-brushedPoints(mtcars,input$mbrush,xvar='wt',yvar='mpg')
+            out <- df %>% 
+                select(wt,mpg)
+            outParam <<- rbind(outParam,out) %>% 
+                dplyr::distinct()
+            return(out)
+        }
+        
     })
-
-output$ggplot_click_xy <- renderPrint({
-    c(input$ggplot_click$x, input$ggplot_click$y)
-})
-
-output$plot_click_option <- renderPlot({
-    plot(mtcars$wt,mtcars$mpg,xlab="wt",ylab="Miler per Galon") 
-})
-
-
-output$all_click_options <- renderPrint({
-    if(!is.null(input$clk$x)){
-        click <- paste0(c('(',round(input$clk$x,2),',',round(input$clk$y,2),')'),collapse = '')
-        click <- paste0("Coordenada del click : ", click,collapse='')
-    } else {click<-NULL}
+    mtcars_plot <- reactive({
+        plot(mtcars$wt,mtcars$mpg, xlab="Precio del vehiculo", ylab = "millas por galon")
+        puntos <-selectedP()
+        if(!is.null(outParamHover)){
+            points(outParamHover[,1],outParamHover[,2],
+                   col='gray',
+                   pch=16,
+                   cex=4)}
+        if(!is.null(outParam)){
+            points(outParam[,1],outParam[,2],
+                   col='green',
+                   pch=16,
+                   cex=2)}
+    })
     
-    
-    if(!is.null(input$dblclick$x)){
-        dblclick <- paste0(c('(',round(input$dblclick$x,2),',',round(input$dblclick$y,2),')'),collapse = '')
-        dblclick <- paste0("Coordenada del doble click : ", dblclick,collapse='')
+    output$plot_click_options <- renderPlot({
         
-    } else{dblclick<-NULL}
+        mtcars_plot()
+    })
     
-    
-    if(!is.null(input$hover$x)){
-        hover <- paste0(c('(',round(input$hover$x,2),',',round(input$hover$y,2),')'),collapse = '')
-        hover <- paste0("Coordenada del cursor : ", hover,collapse='')
-        
-    } else {hover=NULL}
-    
-    
-    if(!is.null(input$brush$xmin)){
-        brushx <- paste0(c('(',round(input$brush$xmin,2),',',round(input$brush$xmax,2),')'),collapse = '')
-        brushy <- paste0(c('(',round(input$brush$ymin,2),',',round(input$brush$ymax,2),')'),collapse = '')
-        brush <- cat('\t rango en x: ', brushx,'\n','\t rango en y: ', brushy)
-        
-    } else {brush<-NULL}
- 
-cat( click,dblclick,hover,brush,sep = "\n" )
-})
 
-
-puntos <- reactive({
-    if(!is.null(input$click_plot_tarea$x)){
-        df<-nearPoints(mtcars,input$click_plot_tarea,xvar='wt',yvar='mpg')
-        out <- df %>% 
-            select(wt,mpg)
-        out_click <<- rbind(out_click,out) %>% distinct()
-        return(out)
-    }
-    if(!is.null(input$hover_plot_tarea$x)){
-        df<-nearPoints(mtcars,input$hover_plot_tarea,xvar='wt',yvar='mpg')
-        out <- df %>% 
-            select(wt,mpg)
-        out_hover <<- out
-        return(out_hover)
-    }
+    output$salida_table <- DT::renderDataTable({
+        #click_table() %>% DT::datatable()
+    })
     
-    if(!is.null(input$dblclck_plot_tarea$x)){
-        df<-nearPoints(mtcars,input$dblclck_plot_tarea,xvar='wt',yvar='mpg')
-        out <- df %>% 
-            select(wt,mpg)
-        out_click <<- setdiff(out_click,out)
-        return(out_hover)
-    }
-    
-    if(!is.null(input$brush_plot_tarea)){
-        df<-brushedPoints(mtcars,input$brush_plot_tarea,xvar='wt',yvar='mpg')
-        out <- df %>% 
-            select(wt,mpg)
-        out_click <<- rbind(out_click,out) %>% dplyr::distinct()
-        return(out_hover)
-    }
-    
-    
-    
-})
-
-
-mtcars_plot <- reactive({
-    plot(mtcars$wt,mtcars$mpg,xlab="wt",ylab="Miles per Galon")
-    puntos <-puntos()
-    if(!is.null(out_hover)){
-        points(out_hover[,1],out_hover[,2],
-               col='gray',
-               pch=16,
-               cex=2)}
-    if(!is.null(out_click)){
-    points(out_click[,1],out_click[,2],
-              col='green',
-              pch=16,
-           cex=2)}
-    
-    
-    
-    
-   
-    
-})
-
-output$plot_tarea <- renderPlot({
-    
-    mtcars_plot()
-})
-
-
-click_table <- reactive({
-    input$click_plot_tarea$x
-    input$dblclck_plot_tarea$x
-    input$brush_plot_tarea
-    out_click
-})
-
-output$tarea_dt <- DT::renderDataTable({
-    click_table() %>% DT::datatable()
-})
-
 })
--- Lab2/ui.R
+++ Lab2/ui.R
@@ -1,39 +1,21 @@
-library(shiny)
 
+library(shiny)
+library(DT)
 
 shinyUI(fluidPage(
 
-    
-    titlePanel("Old Faithful Geyser Data"),
+    # Application title
+    titlePanel("Tarea Shiny Plots"),
 
-    tabsetPanel(tabPanel("plot",
-                         h1("Graficas en shiny"),
-                         plotOutput("grafica_base_r"),
-                         plotOutput("grafica_ggplot")
-                         ),
-                tabPanel("plot click",
-                         plotOutput("click_base_plot",click="plot_click"),
-                         verbatimTextOutput("click_base_plot_xy"),
-                         plotOutput("click_ggplot",click="ggplot_click"),
-                         verbatimTextOutput("ggplot_click_xy")
-                         ),
-                tabPanel("Todas las opciones",
-                         plotOutput("plot_click_option",
-                                    click = 'clk',
-                                    dblclick = 'dblclick',
-                                    hover = 'hover',
-                                    brush = 'brush'   ),
-                         verbatimTextOutput("all_click_options")
-                         ),
-                tabPanel('Tarea',
-                         plotOutput('plot_tarea',
-                                    click = 'click_plot_tarea',
-                                    dblclick = 'dblclck_plot_tarea',
-                                    hover = 'hover_plot_tarea',
-                                    brush = 'brush_plot_tarea'
-                                    ),
-                         DT::dataTableOutput('tarea_dt')
-                         )
-                )
-    
+    tabsetPanel(
+
+        tabPanel("Tarea",
+                 plotOutput("plot_click_options",
+                            click = "clk",
+                            dblclick = "dclk",
+                            hover = "mhover",
+                            brush = "mbrush"),
+                 DT::dataTableOutput("salida_table")
+        )
+    )
 ))
